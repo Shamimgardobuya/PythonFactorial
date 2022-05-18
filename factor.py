6b59d71bc788c97fcaf4ad131b1b3649e13edb5d
Shamimgardobuya <shamimobuya@gmail.com>
2022-05-18 21:50:25 +0300

    made a factor

--- factor.py
+++ factor.py
@@ -1,13 +1,27 @@
 
 
 
-num=int(input("Enter a number that finds the facorial"))
-b=1
-if num<0:
-        print("There is no factor of negative number")
-elif num==0:
-        print("The factor is 1")
-else:
-    for i in range(1,num+1):  # i think this is the increament.#why is it a zero.
-               b*=i
-               print(f"The factorial of {num} is {b}")  # would like output to be just one value
+# num=int(input("Enter a number that finds the facorial"))
+# b=1
+# if num<0:
+#         print("There is no factor of negative number")
+# elif num==0:
+#         print("The factor is 1")
+# else:
+#     for i in range(1,num+1):  # i think this is the increament.#why is it a zero.
+#                b*=i
+# print(f"The factorial of {num} is {b}")  # would like output to be just one value
+
+def factorial(num):
+        b=1
+        if num<0:
+                print("There is no factor of negative number")
+        elif num==0:
+                print("The factor is 1")
+        else:
+                for i in range(1,num+1):  # i think this is the increament.#why is it a zero.
+                        b*=i #i have moved away from block b hence output correct.
+        print(f"The factorial of {num} is {b}")  # would like output to be just one value
+
+nums=int(input("Enter a number that finds the facorial"))
+factorial(num=nums)
